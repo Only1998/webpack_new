7f70b883e6ba741e4303bb20c2fe58af436465a1
only <only98@126.com>
2020-12-09 18:50:16 +0800

    01

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -1,12 +1,90 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const miniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
-  mode: "production",
+  mode: "production", //development production
   entry: {
     index: "./src/index.js",
+    app: "./src/app.js",
   },
   output: {
     path: path.resolve(__dirname, "../dist/"),
     // filename: 'bundle.js' //单个入口
-    filename: "[name].[hash].js",
+    filename: "[name].js",
   },
+  devServer: {
+    
+  }
+  module: {
+    rules: [
+      //css
+      {
+        test: /\.css$/,
+        use: [
+          { loader: miniCssExtractPlugin.loader },
+          { loader: "css-loader" },
+        ],
+      },
+      //less
+      {
+        test: /\.less$/,
+        use: [
+          { loader: miniCssExtractPlugin.loader },
+          { loader: "css-loader" },
+          { loader: "less-loader" },
+        ],
+      },
+      //图片
+      // {
+      //   test: /\.(jpg|png|gif|webp|jpeg)$/,
+      //   use: [{ loader: "file-loader" }],
+      // },
+      //base64编码格式图片
+      {
+        test: /\.(jpg|png|gif|webp|jpeg)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit: 102400, //图片小于100k时转化为base64
+            },
+          },
+        ],
+      },
+      //es6==>es5
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["env"],
+            },
+          },
+        ],
+      },
+    ],
+  },
+  //打包HTML
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "网页标题", // 需要在模板文件中的title填写为：<%= htmlWebpackPlugin.options.title %>
+      template: "./src/tpl.html",
+      inject: "body",
+      // body： script标签位于html文件的body底部；
+      // head： script标签位于html文件的head中；
+      // false：不插入生成的js文件。
+      minify: {
+        // html压缩规则
+        removeComments: true, // 是否移除注释
+        removeAttributeQuotes: true, // 是否省略标签属性的引号
+        collapseWhitespace: true, // 是否删除文件的空白字符
+      },
+      filename: "index.html", // 输出模板名称, 默认为index.html
+    }),
+    new miniCssExtractPlugin({
+      filename: "[name].[hash].css",
+    }),
+  ],
 };
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import jpg1 from "./1.jpg";
+import jpg2 from "./2.jpg";
+import index from "./index.css";
+import indexLess from "./index.less";
+//
+var img1 = document.createElement("img");
+img1.src = jpg1;
+document.body.appendChild(img1);
+//
+var img2 = document.createElement("img");
+img2.src = jpg2;
+document.body.appendChild(img2);
+//
+index;
+indexLess;
+//
